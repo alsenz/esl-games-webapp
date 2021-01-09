232877ed65a0daeca91c61323dbc562332ed0759
thomasplowman <tomplowy@gmail.com>
2021-01-09 23:22:40 +0000

    emergency commit

--- src/PresentationApp.js
+++ src/PresentationApp.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Reveal from 'reveal.js';
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/league.css';
 import './PresentationApp.css';
 
-function PresentationApp() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the Presentation App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class PresentationApp extends React.Component {
+    
+    constructor(props) {
+      super(props);
+      this.state = {
+        dummyList: ["Point 1"]
+      };
+    }
+    
+    componentDidMount() {
+      console.log("COMPONENT DID MOUNT");
+      this.deck = new Reveal({
+        plugins: [ RevealMarkdown ],
+        controls: true,
+        embedded: true,
+        navigationMode: "linear"
+      });
+      this.deck.initialize();
+      console.log("deck: ", this.deck);
+      console.dir(this.deck);
+      setTimeout(() => {
+        console.log("Timeout expired...");
+        this.setState( prevState => ({
+          dummyList: prevState.dummyList.concat(["Another item..."])
+        }));
+      }, 5000);
+    }
+
+  
+    componentDidUpdate() {
+      console.log("COMPONENT DID UPDATE...");
+      this.deck.layout();
+      this.deck.next();
+    }
+    
+    render() {
+      console.log("RENDER");
+      let contentList = this.state.dummyList.map((item) => <li key={item} className="fragment">{item}</li>);
+      return (
+        <div className="reveal">
+          <div className="slides">
+            <section>Registration</section>
+            <section data-transition="convex" data-markdown="">
+              <textarea data-template>
+## Content 1 Title
+A paragraph with some text and a [link](http://hakim.se).
+              </textarea>
+            </section>
+            <section data-transition="concave">
+              <h3>Content 2</h3>
+              <ul> { contentList } </ul>
+            </section>
+            <section>Credits</section>
+          </div>
+        </div>
+      );
+    }
+
 }
 
 export default PresentationApp;
